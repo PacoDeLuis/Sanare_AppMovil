171acae40c46841c5e762568aeb103cb6401e28f
Laura <lauracontreras19102003@gmail.com>
2025-11-03 21:53:38 -0600

    integración de Home

---
+++ lib/Home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'esto es home',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
